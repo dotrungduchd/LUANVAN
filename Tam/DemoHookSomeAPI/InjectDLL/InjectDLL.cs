beeb2a2ce26549183d87e434ac58f197225aa767
tqtam <tqtam1993@gmail.com>
2015-03-21 18:15:21 +0700

    Add GetFileNameFromHandle Hook WriteFile

--- Tam/DemoHookSomeAPI/InjectDLL/InjectDLL.cs
+++ Tam/DemoHookSomeAPI/InjectDLL/InjectDLL.cs
@@ -14,7 +14,7 @@ namespace InjectDLL
     public class Main: IEntryPoint
     {
         FileMonInterface Interface;
-        LocalHook TextOutHook;        
+        LocalHook LocalTestHook;        
         Stack<String> Queue = new Stack<String>();
 
         public Main(
@@ -34,12 +34,12 @@ namespace InjectDLL
             // install hook...
             try
             {                
-                TextOutHook = LocalHook.Create(
-                    LocalHook.GetProcAddress("kernel32.dll", "CreateFileW"),
-                    new DCreateFile(CreateFile_Hooked),
-                    this);                
+                LocalTestHook = LocalHook.Create(
+                    LocalHook.GetProcAddress("kernel32.dll", "WriteFile"),
+                    new DWriteFile(WriteFile_Hooked),
+                    this);
 
-                TextOutHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
+                LocalTestHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
             }
             catch (Exception ExtInfo)
             {
@@ -120,6 +120,13 @@ namespace InjectDLL
             UInt32 InFlagsAndAttributes,
             IntPtr InTemplateFile);
 
+        [UnmanagedFunctionPointer(CallingConvention.StdCall,
+            CharSet = CharSet.Unicode,
+            SetLastError = true)]
+        delegate bool DWriteFile(IntPtr hFile, byte[] lpBuffer,
+   uint nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten,
+   [In] ref System.Threading.NativeOverlapped lpOverlapped);
+
         #endregion
 
         #region KhaiBaoAPI
@@ -151,6 +158,11 @@ namespace InjectDLL
             UInt32 InCreationDisposition,
             UInt32 InFlagsAndAttributes,
             IntPtr InTemplateFile);
+
+        [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true, CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode)]
+        public static extern bool WriteFile(IntPtr hFile, byte[] lpBuffer,
+   uint nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten,
+   [In] ref System.Threading.NativeOverlapped lpOverlapped);
 
         #endregion
         
@@ -322,6 +334,136 @@ namespace InjectDLL
                 InTemplateFile);
         }
 
+        static bool WriteFile_Hooked(IntPtr hFile, byte[] lpBuffer,
+                uint nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten,
+                [In] ref System.Threading.NativeOverlapped lpOverlapped)
+        {
+
+            byte[] bytes = new byte[nNumberOfBytesToWrite];
+            for (uint i = 0; i < nNumberOfBytesToWrite; i++)
+            {
+                bytes[i] = Marshal.ReadByte(lpBuffer, (int)i);
+            }
+            Console.WriteLine(nNumberOfBytesToWrite);
+            
+            try
+            {
+
+                Main This = (Main)HookRuntimeInfo.Callback;
+                lock (This.Queue)
+                {
+                    This.Queue.Push("[" + RemoteHooking.GetCurrentProcessId() + ":" +
+                        RemoteHooking.GetCurrentThreadId() + "]:" + bytes.Length.ToString() +"-" +GetFileNameFromHandle(hFile));
+
+                }
+            }
+            catch
+            {
+            }
+
+            // call original API...
+            return WriteFile(hFile, bytes, nNumberOfBytesToWrite, out lpNumberOfBytesWritten, ref lpOverlapped);
+        }
+
+        #endregion
+
+
+        #region FindPathHelper
+        [DllImport("kernel32.dll")]
+        static extern uint GetFileSize(IntPtr hFile, IntPtr lpFileSizeHigh);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern IntPtr CreateFileMapping(
+            IntPtr hFile,
+            IntPtr lpFileMappingAttributes,
+            FileMapProtection flProtect,
+            uint dwMaximumSizeHigh,
+            uint dwMaximumSizeLow,
+            [MarshalAs(UnmanagedType.LPTStr)]string lpName);
+
+        [Flags]
+        public enum FileMapProtection : uint
+        {
+            PageReadonly = 0x02,
+            PageReadWrite = 0x04,
+            PageWriteCopy = 0x08,
+            PageExecuteRead = 0x20,
+            PageExecuteReadWrite = 0x40,
+            SectionCommit = 0x8000000,
+            SectionImage = 0x1000000,
+            SectionNoCache = 0x10000000,
+            SectionReserve = 0x4000000,
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr MapViewOfFile(
+            IntPtr hFileMappingObject,
+            FileMapAccess dwDesiredAccess,
+            uint dwFileOffsetHigh,
+            uint dwFileOffsetLow,
+            uint dwNumberOfBytesToMap);
+
+        [Flags]
+        public enum FileMapAccess : uint
+        {
+            FileMapCopy = 0x0001,
+            FileMapWrite = 0x0002,
+            FileMapRead = 0x0004,
+            FileMapAllAccess = 0x001f,
+            fileMapExecute = 0x0020,
+        }
+
+        [DllImport("psapi.dll", SetLastError = true)]
+        public static extern uint GetMappedFileName(IntPtr m_hProcess, IntPtr lpv, StringBuilder
+                lpFilename, uint nSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool UnmapViewOfFile(IntPtr lpBaseAddress);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        public static string GetFileNameFromHandle(IntPtr FileHandle)
+        {
+            string fileName = String.Empty;
+            IntPtr fileMap = IntPtr.Zero, fileSizeHi = IntPtr.Zero;
+            UInt32 fileSizeLo = 0;
+
+            fileSizeLo = GetFileSize(FileHandle, fileSizeHi);
+
+            if (fileSizeLo == 0)
+            {
+                // cannot map an 0 byte file
+                return "Empty file.";
+            }
+
+            fileMap = CreateFileMapping(FileHandle, IntPtr.Zero, FileMapProtection.PageReadonly, 0, 1, null);
+
+            if (fileMap != IntPtr.Zero)
+            {
+                IntPtr pMem = MapViewOfFile(fileMap, FileMapAccess.FileMapRead, 0, 0, 1);
+                if (pMem != IntPtr.Zero)
+                {
+                    StringBuilder fn = new StringBuilder(250);
+                    GetMappedFileName(System.Diagnostics.Process.GetCurrentProcess().Handle, pMem, fn, 250);
+                    if (fn.Length > 0)
+                    {
+                        UnmapViewOfFile(pMem);
+                        CloseHandle(FileHandle);
+                        return fn.ToString();
+                    }
+                    else
+                    {
+                        UnmapViewOfFile(pMem);
+                        CloseHandle(FileHandle);
+                        return "Empty filename.";
+                    }
+                }
+            }
+
+            return "Empty filemap handle.";
+        }
         #endregion
     }
 }
