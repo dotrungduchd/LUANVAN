ed9ac8d8ac54d6c1e37cc98c5f3acc6bc6307f0f
tqtam <tqtam1993@gmail.com>
2015-05-07 16:23:54 +0700

    CreateProcess

--- Tam/DemoHookSomeAPI/DemoHookSomeAPI/Program.cs
+++ Tam/DemoHookSomeAPI/DemoHookSomeAPI/Program.cs
@@ -1,7 +1,10 @@
 ﻿using EasyHook;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,15 +31,72 @@ namespace DemoHookSomeAPI
             Console.WriteLine("The target process has reported an error:\r\n" + InInfo.ToString());
         }
 
+        public void OnCreateProcess(Int32 InClientPID, Int32[] pIds,int oldThrId,ThreadPriorityLevel oldThrLvl ,string ChannelName)
+        {
+            for (int i = 0; i < pIds.Length; i++)
+            {
+                try
+                {
+                    RemoteHooking.Inject(pIds[i],
+                        "InjectDLL.dll",
+                        "InjectDLL.dll",
+                        ChannelName);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                Process pro = Process.GetProcessById(pIds[i]);
+                foreach (ProcessThread thread in pro.Threads)
+                {
+                    if(thread.Id == oldThrId)
+                        thread.PriorityLevel = oldThrLvl;
+                }
+            }
+        }
+
+        public string[] getExtensions()
+        {
+            return Global.extensions;
+        }
+
+        //public void OnCreateProcess(Int32 InClientPID, string lpApplicationName, string lpCommandLine, string ChannelName)
+        //{
+        //    PROCESS_INFORMATION pInfo = new PROCESS_INFORMATION();
+        //    STARTUPINFO sInfo = new STARTUPINFO();
+        //    SECURITY_ATTRIBUTES pSec = new SECURITY_ATTRIBUTES();
+        //    SECURITY_ATTRIBUTES tSec = new SECURITY_ATTRIBUTES();
+        //    try
+        //    {
+        //        //bool retValue = GlobalHelper.CreateProcess(lpApplicationName, lpCommandLine,
+        //        //ref pSec, ref tSec, false, 0x00000004,
+        //        //IntPtr.Zero, null, ref sInfo, out pInfo);
+        //        int outProcessID = 0;
+        //        RemoteHooking.CreateAndInject(lpApplicationName, lpCommandLine, (int)0x00000004, "InjectDLL.dll", "InjectDLL.dll", out outProcessID, ChannelName);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.ToString());
+        //    }
+        //}
+          
         public void Ping()
         {
         }
+
+    }
+    public class Global
+    {
+        public static string[] extensions = { ".rtf", ".txt",".docx",".doc",".mp3",".png",".jpg",".bmp",".flv",".jpeg",".exe",".mkv",".pdf",
+                                   ".xls",".cs",".xlsx",".ppt",".pptx"};
     }
     class Program
     {
-        static String ChannelName = null;
+        static String ChannelName = null;        
+        
         static void Main(string[] args)
         {
+            
             Int32 TargetPID = 0;
 
             if ((args.Length != 1) || !Int32.TryParse(args[0], out TargetPID))
@@ -55,15 +115,16 @@ namespace DemoHookSomeAPI
                     "DemoHookSomeAPI.exe",
                     "InjectDLL.dll");
 
-                RemoteHooking.IpcCreateServer<FileMonInterface>(ref ChannelName, WellKnownObjectMode.SingleCall);
+                RemoteHooking.IpcCreateServer<FileMonInterface>(ref ChannelName, WellKnownObjectMode.Singleton);
 
-                //RemoteHooking.InstallSupportDriver();
+                //RemoteHooking.InstallSupportDriver();                
+
                 RemoteHooking.Inject(
                     TargetPID,
                     "InjectDLL.dll",
                     "InjectDLL.dll",
                     ChannelName);
-
+                
                 Console.ReadLine();
             }
             catch (Exception ExtInfo)
--- Tam/DemoHookSomeAPI/InjectDLL/InjectDLL.cs
+++ Tam/DemoHookSomeAPI/InjectDLL/InjectDLL.cs
@@ -10,22 +10,29 @@ using System.Threading;
 using System.Runtime.InteropServices;
 using System.Drawing;
 using System.IO;
+using System.Diagnostics;
 
 namespace InjectDLL
 {
     public class Main : IEntryPoint
     {
         FileMonInterface Interface;
-        LocalHook WriteFileHook, CreateFileHook, ReadFileHook;
+        LocalHook WriteFileHook, CreateFileHook, ReadFileHook, CreateProcessHook;
         Stack<String> Queue = new Stack<String>();
+        Stack<Int32> pIdQueue = new Stack<Int32>();
+        Dictionary<string, object> cpParams = new Dictionary<string, object>();
+        IntPtr tmpThread = IntPtr.Zero;
+        ThreadPriorityLevel thrOldLvl;
+        int oldThrId = 0;
+        String myChannelName;
 
         public Main(
              RemoteHooking.IContext InContext,
              String InChannelName)
         {
             // connect to host...
-            Interface = RemoteHooking.IpcConnectClient<FileMonInterface>(InChannelName);
-
+            Interface = RemoteHooking.IpcConnectClient<FileMonInterface>(InChannelName);            
+            myChannelName = InChannelName;
             Interface.Ping();
         }
 
@@ -36,10 +43,13 @@ namespace InjectDLL
             // install hook...
             try
             {
+                
                 WriteFileHook = LocalHook.Create(
                     LocalHook.GetProcAddress("kernel32.dll", "WriteFile"),
                     new DWriteFile(WriteFile_Hooked),
                     this);
+
+                WriteFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
 
                 //CreateFileHook = LocalHook.Create(
                 //    LocalHook.GetProcAddress("kernel32.dll", "CreateFileW"),
@@ -52,7 +62,11 @@ namespace InjectDLL
 
                 ReadFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
 
-                WriteFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
+                CreateProcessHook = LocalHook.Create(LocalHook.GetProcAddress("kernel32.dll", "CreateProcessW"),
+                    new DCreateProcess(CreateProcess_Hooked),
+                    this);
+
+                CreateProcessHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
                 //CreateFileHook.ThreadACL.SetExclusiveACL(new Int32[] { 0 });
             }
             catch (Exception ExtInfo)
@@ -75,6 +89,36 @@ namespace InjectDLL
                     Thread.Sleep(500);
 
                     // transmit newly monitored file accesses...
+
+                    if (pIdQueue.Count > 0)
+                    {
+                        Int32[] Package = null;
+                        lock (pIdQueue)
+                        {
+                            Package = pIdQueue.ToArray();
+                            pIdQueue.Clear();
+                        }
+                        Interface.OnCreateProcess(RemoteHooking.GetCurrentProcessId(), Package,oldThrId,thrOldLvl ,this.myChannelName);
+                    }
+                    //if (cpParams.Count > 2)
+                    //{
+                    //    string lpApplicationName = "";
+                    //    string lpCommandline = "";
+                    //    uint dwCreationFlags = 0;
+                    //    lock (cpParams)
+                    //    {
+                    //        lpApplicationName = (string)cpParams["ApplicationName"];
+                    //        lpCommandline = (string)cpParams["CommandLine"];
+                    //        dwCreationFlags = (uint)cpParams["CreationFlags"];
+                    //        cpParams.Clear();
+                    //    }
+                    //    if (!string.IsNullOrEmpty(lpApplicationName))
+                    //    {
+                    //        Interface.OnCreateProcess(RemoteHooking.GetCurrentProcessId(), lpApplicationName, lpCommandline, this.myChannelName);
+                    //    }
+                    //}
+
+
                     if (Queue.Count > 0)
                     {
                         String[] Package = null;
@@ -90,10 +134,13 @@ namespace InjectDLL
                     }
                     else
                         Interface.Ping();
+
+
                 }
             }
-            catch
+            catch(Exception ex)
             {
+                Interface.ReportException(ex);
                 // Ping() will raise an exception if host is unreachable
             }
         }
@@ -148,6 +195,21 @@ namespace InjectDLL
         delegate bool DReadFile(IntPtr hFile, IntPtr lpBuffer,
            uint nNumberOfBytesToRead, IntPtr lpNumberOfBytesRead, [In] ref System.Threading.NativeOverlapped lpOverlapped);
 
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall,
+            CharSet = CharSet.Unicode,
+            SetLastError = true)]
+        delegate bool DCreateProcess(string lpApplicationName,
+            string lpCommandLine,
+            IntPtr lpProcessAttributes,
+            IntPtr lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref STARTUPINFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation);
+
         #endregion
 
         #region KhaiBaoAPI
@@ -198,6 +260,31 @@ namespace InjectDLL
         [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
         public static extern void OutputDebugString(string message);
 
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool CreateProcess(
+           string lpApplicationName,
+           string lpCommandLine,
+           IntPtr lpProcessAttributes,
+           IntPtr lpThreadAttributes,
+           bool bInheritHandles,
+           uint dwCreationFlags,
+           IntPtr lpEnvironment,
+           string lpCurrentDirectory,
+           [In] ref STARTUPINFO lpStartupInfo,
+           out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint ResumeThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
 
         #endregion
 
@@ -247,6 +334,66 @@ namespace InjectDLL
             PAGE_WRITECOMBINE = 0x400
         }
 
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr lpSecurityDescriptor;
+            public int bInheritHandle;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFOEX
+        {
+            public STARTUPINFO StartupInfo;
+            public IntPtr lpAttributeList;
+        }
+
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
 
         #endregion
 
@@ -425,6 +572,8 @@ namespace InjectDLL
             return WriteFile(hFile, lpBuffer, nNumberOfBytesToWrite, out lpNumberOfBytesWritten, ref lpOverlapped);
         }
 
+
+        // Read data from pointer
         private static byte[] ReadDataFromPointer(IntPtr lpBuffer, uint nNumberOfBytesToWrite)
         {
             byte[] bytes = new byte[nNumberOfBytesToWrite];
@@ -444,29 +593,31 @@ namespace InjectDLL
             StringBuilder fnPath = new StringBuilder((int)MAX_PATH);
             GetFinalPathNameByHandle(hFile, fnPath, MAX_PATH, FILE_NAME_NORMALIZED);
             bool ReturnValue = false;
-            string filePath = fnPath.ToString();
-            if ((!filePath.Contains("Encryption")) || (!CheckExtension(filePath)))
-            {                
+            string filePath = fnPath.ToString();            
+            Main This = (Main)HookRuntimeInfo.Callback;
+            string[] exts = This.Interface.getExtensions();
+            if ((!filePath.Contains("Encryption")) || (!CheckExtension(filePath,exts)))
+            {
                 return ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, ref lpOverlapped);
             }
             else
             {
                 try
                 {
-                    Main This = (Main)HookRuntimeInfo.Callback;
-                    ReturnValue = This.ReadFileFunc(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, ref lpOverlapped);
-                    OutputDebugString("Da goi duoc API goc - " + nNumberOfBytesToRead );
+                    
+                    ReturnValue = This.ReadFileFunc(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, ref lpOverlapped);                    
                     int bytesCount = (int)nNumberOfBytesToRead;
-                    if (!lpNumberOfBytesRead.Equals(IntPtr.Zero))
+                    if (!lpNumberOfBytesRead.Equals(IntPtr.Zero) && Marshal.ReadInt32(lpNumberOfBytesRead)!=0)
                     {
-                        bytesCount = Marshal.ReadInt32(lpNumberOfBytesRead);
+                        bytesCount = Marshal.ReadInt32(lpNumberOfBytesRead);                        
                     }
                     else
                     {
                         bytesCount = lpOverlapped.InternalHigh.ToInt32();
+                        OutputDebugString("Co overlapped -"+bytesCount);
                     }
-                    byte[] bytes = ReadDataFromPointer(lpBuffer,(uint) bytesCount);                   
-                    bool ValidExtension = CheckExtension(filePath);
+                    byte[] bytes = ReadDataFromPointer(lpBuffer, (uint)bytesCount);                    
+                    bool ValidExtension = CheckExtension(filePath,exts);
                     if (ValidExtension)
                     {
                         for (uint i = 0; i < bytesCount; i++)
@@ -474,13 +625,12 @@ namespace InjectDLL
                             bytes[i] -= 1;
                         }
                         Marshal.Copy(bytes, 0, lpBuffer, (int)bytesCount);
-                    }
-                    OutputDebugString("Da thay doi du lieu");
+                    }                    
                     lock (This.Queue)
                     {
                         if (ValidExtension)
                         {
-                            DateTime now = DateTime.Now;
+                            DateTime now = DateTime.Now;                            
                             This.Queue.Push("[" + RemoteHooking.GetCurrentProcessId() + "-ReadFile:" +
                                 RemoteHooking.GetCurrentThreadId() + ":" + bytesCount + "]:" + "-" + fnPath + "\n");
                         }
@@ -494,9 +644,117 @@ namespace InjectDLL
             }
         }
 
-        private static bool CheckExtension(string filePath)
+        static bool CreateProcess_Hooked(string lpApplicationName,
+           string lpCommandLine,
+           IntPtr lpProcessAttributes,
+           IntPtr lpThreadAttributes,
+           bool bInheritHandles,
+           uint dwCreationFlags,
+           IntPtr lpEnvironment,
+           string lpCurrentDirectory,
+           [In] ref STARTUPINFO lpStartupInfo,
+           out PROCESS_INFORMATION lpProcessInformation)
         {
-            return filePath.Contains(".rtf") || filePath.Contains(".txt") || filePath.Contains(".docx") || filePath.Contains(".doc") || filePath.Contains(".bmp") || filePath.Contains(".png") || filePath.Contains(".exe") || filePath.Contains(".jpg") || filePath.Contains(".mp3");
+            PROCESS_INFORMATION proInformation = new PROCESS_INFORMATION();
+            bool ReturnValue = false;
+            int processId = 0;
+            try
+            {
+                
+                Main This = (Main)HookRuntimeInfo.Callback;                
+                
+                string InLibraryPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(typeof(FileMonInterface).Assembly.Location), "InjectDLL.dll");
+                //dwCreationFlags = dwCreationFlags | 0x00000004;
+                
+                ReturnValue = CreateProcess(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, false,dwCreationFlags, lpEnvironment, null, ref lpStartupInfo, out proInformation);
+                Process pro = Process.GetProcessById(proInformation.dwProcessId);
+                This.oldThrId = proInformation.dwThreadId;
+                foreach (ProcessThread thread in pro.Threads)
+                {
+                    if (thread.Id == proInformation.dwThreadId)
+                    {
+                        This.thrOldLvl = thread.PriorityLevel;
+                        thread.PriorityLevel = ThreadPriorityLevel.Idle;
+                    }
+                }
+                
+
+
+                //OutputDebugString(InLibraryPath);
+                //RemoteHooking.CreateAndInject(lpApplicationName, lpCommandLine, (int)dwCreationFlags,
+                //    InLibraryPath, // 32-bit version (the same because AnyCPU)
+                //    InLibraryPath,
+                //    out processId,
+                //    This.myChannelName);
+                
+                //HelperServiceInterface helper = new HelperServiceInterface();
+                //helper.InjectEx(
+                //    NativeAPI.GetCurrentProcessId(),
+                //    proInformation.dwProcessId,
+                //    proInformation.dwThreadId,
+                //    0x20000000,
+                //    "InjectDLL.dll",
+                //    "InjectDLL.dll",
+                //    true,
+                //    false,
+                //    false,
+                //    This.myChannelName);
+                //SuspendThread(proInformation.hThread); 
+                lock (This.Queue)
+                {
+                    DateTime now = DateTime.Now;
+                    This.Queue.Push("[" + RemoteHooking.GetCurrentProcessId() + "-CreateProcess:" +
+                        RemoteHooking.GetCurrentThreadId() + "]:" + "-" + lpApplicationName + proInformation.dwProcessId + "\n");
+
+                }
+                lock (This.pIdQueue)
+                {
+                    This.pIdQueue.Push(proInformation.dwProcessId);
+                }
+                //lock (This.cpParams)
+                //{
+                //    if (!This.cpParams.Keys.Contains("ApplicationName"))
+                //    {
+                //        This.cpParams.Add("ApplicationName", lpApplicationName);
+                //    }
+                //    if (!This.cpParams.Keys.Contains("CommandLine"))
+                //    {
+                //        This.cpParams.Add("CommandLine", lpCommandLine);
+                //    }
+                //    if (!This.cpParams.Keys.Contains("CreationFlags"))
+                //    {
+                //        This.cpParams.Add("CreationFlags", dwCreationFlags);
+                //    }
+                //}                
+
+
+            }
+            catch (Exception ex)
+            {
+                OutputDebugString(ex.Message);
+            }
+            lpProcessInformation = new PROCESS_INFORMATION();
+            lpProcessInformation.dwProcessId = proInformation.dwProcessId;
+            lpProcessInformation.dwThreadId = proInformation.dwThreadId;
+            lpProcessInformation.hProcess = proInformation.hProcess;
+            lpProcessInformation.hThread = proInformation.hThread;            
+            //ReturnValue = processId != 0;
+
+            return ReturnValue;
+
+        }
+        static string[] strExtensions = { ".rtf", ".txt",".docx",".doc",".mp3",".png",".jpg",".bmp",".flv",".jpeg",".exe",".mkv",".pdf",
+                                   ".xls",".cs",".xlsx"};
+
+        private static bool CheckExtension(string filePath,string[] exts)
+        {               
+            foreach (var ext in exts)
+            {
+                
+                if (filePath.Contains(ext))
+                    return true;
+            }
+            return false;
         }
 
         #endregion
