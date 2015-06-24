6e548b593a661e29565dbe132add3f8c98933c0b
dotrungduchd <dotrungduchd@gmail.com>
2015-06-24 07:44:30 +0700

    # Update ActiveDomain.cs

--- Duc/ActiveDomain/ActiveDomain/Program.cs
+++ Duc/ActiveDomain/ActiveDomain/Program.cs
@@ -16,26 +16,43 @@ namespace ActiveDomain
             // Get current User login on Windows
             string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
             Console.WriteLine(userName);
+            // print: DOMAIN/tamtq
+
+            // Get current Domain
+            //string domain = Environment.UserDomainName;
+            //Console.WriteLine(domain);
+            // print: DOMAIN
+
+            // Get current full Domain 
+            string domain = System.Net.NetworkInformation.IPGlobalProperties.GetIPGlobalProperties().DomainName;
+            Console.WriteLine(domain);
             Console.ReadKey();
+            // print: domain.local
 
             
 
             // Get all user in domain
             // Run in any windows client
-            using (var context = new PrincipalContext(ContextType.Domain, "domain.local"))
+            try
             {
-                using (var searcher = new PrincipalSearcher(new UserPrincipal(context)))
+                using (var context = new PrincipalContext(ContextType.Domain, domain))
                 {
-                    foreach (var result in searcher.FindAll())
+                    using (var searcher = new PrincipalSearcher(new UserPrincipal(context)))
                     {
-                        DirectoryEntry de = result.GetUnderlyingObject() as DirectoryEntry;
-                        Console.WriteLine("First Name: " + de.Properties["givenName"].Value);
-                        Console.WriteLine("Last Name : " + de.Properties["sn"].Value);
-                        Console.WriteLine("SAM account name   : " + de.Properties["samAccountName"].Value);
-                        Console.WriteLine("User principal name: " + de.Properties["userPrincipalName"].Value);
-                        Console.WriteLine();
+                        foreach (var result in searcher.FindAll())
+                        {
+                            DirectoryEntry de = result.GetUnderlyingObject() as DirectoryEntry;
+                            Console.WriteLine("First Name: " + de.Properties["givenName"].Value);
+                            Console.WriteLine("Last Name : " + de.Properties["sn"].Value);
+                            Console.WriteLine("SAM account name   : " + de.Properties["samAccountName"].Value); // print: tamtq
+                            Console.WriteLine("User principal name: " + de.Properties["userPrincipalName"].Value); //print: tamtq@domain.local
+                            Console.WriteLine();
+                        }
                     }
                 }
+            }
+            catch(Exception)
+            {
             }
             Console.ReadLine();
 
