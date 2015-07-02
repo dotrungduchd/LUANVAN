343c3d11bfa9de03e0d812df1372facb33b0a266
dotrungduchd <dotrungduchd@gmail.com>
2015-07-02 20:21:04 +0700

    # Update code # Move Signer.cs to InjectDLL project # Update Form, Signer.cs, Global.cs

--- Tam/DemoHackingApp/Forms/AuthenticationForm.cs
+++ Tam/DemoHackingApp/Forms/AuthenticationForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using InjectDLL;
 
 namespace DemoHackingApp
 {
@@ -14,13 +15,37 @@ namespace DemoHackingApp
         public AuthenticationForm()
         {
             InitializeComponent();
+
+            // Load Previous Setting
+            if (Global.AUTH_TYPE == (int)AuthType.Personal)
+            {
+                pnDomain.Enabled = false;
+                rbPersonnal.Checked = true;
+
+                if (Config.GetAppSetting(Config.REMEMBER_ME) == "True")
+                    isRememberMe = true;
+                if (isRememberMe)
+                {
+                    tbID.Text = Global.ID;
+                    tbPassword.Text = Global.PASSWORD;
+                    cbRememberMe.Checked = isRememberMe;
+                }
+            }
+            else
+            {
+                pnPersonal.Enabled = false;
+                rbDomain.Checked = true;
+
+                rbOnlyMe.Checked = Global.DOMAIN_PERMIT == (int)DomainPermission.OnlyMe;
+                rbAllUserDomain.Checked = Global.DOMAIN_PERMIT == (int)DomainPermission.AllUserInDomain;
+                rbSomeOneDomain.Checked = Global.DOMAIN_PERMIT == (int)DomainPermission.SomeUserInDomain;
+                DomainPermit = Global.DOMAIN_PERMIT;
+            }
+
         }
 
         private void AuthenticationForm_Load(object sender, EventArgs e)
         {
-            if (Config.GetAppSetting(Config.REMEMBER_ME) == "True")
-                isRememberMe = true;
-            cbRememberMe.Enabled = isRememberMe;
 
 
         }
@@ -35,7 +60,7 @@ namespace DemoHackingApp
         bool isRememberMe = false;
         private void cbRememberMe_CheckedChanged(object sender, EventArgs e)
         {
-            isRememberMe = cbRememberMe.Enabled;
+            isRememberMe = cbRememberMe.Checked;
 
         }
 
@@ -46,35 +71,59 @@ namespace DemoHackingApp
         /// <param name="e"></param>
         private void btAuth_Click(object sender, EventArgs e)
         {
-            // Validate New User
-            if (Users.UsersList.Keys.Contains(tbID.Text))
+            if (rbPersonnal.Checked == true)
             {
-                if (Users.UsersList[tbID.Text] != Encoding.ASCII.GetString(Signer.Hash(tbPassword.Text)))
+                Global.AUTH_TYPE = (int)AuthType.Personal;
+
+                #region Persional
+                // Validate
+                if (tbID.Text == "" || tbPassword.Text == "")
                 {
-                    MessageBox.Show("User is exist but password incorrect");
+                    MessageBox.Show("ID and Password can not be null");
                     return;
                 }
-                else
+
+                if (tbID.Text.Length < 3 || tbPassword.Text.Length < 3)
                 {
-                    MessageBox.Show("Welcome back " + tbID.Text);
+                    MessageBox.Show("ID and Password length at least 4 charactor");
+                    return;
                 }
-            }
-            else
-            {
-                MessageBox.Show("Welcome " + tbID.Text + ". You must remember your ID and password to decrypt your file");
+
+                // Update Global ID Password variable
+                Global.ID = Global.KEY01 = tbID.Text;
+                Global.PASSWORD = Global.KEY02 = tbPassword.Text;
+                Global.REMEMBER_ME = isRememberMe;
 
                 // Save ID+password if app is remember info
-                Users.AddNewUser(tbID.Text, Encoding.ASCII.GetString(Signer.Hash(tbPassword.Text)));
+                Config.AddAppSetting(Config.ID, Global.ID);
+                Config.AddAppSetting(Config.PASSWORD, Global.PASSWORD);
+                Config.UpdateAppSetting(Config.REMEMBER_ME, isRememberMe.ToString());
+                #endregion
             }
+            else if (rbDomain.Checked == true)
+            {
+                Global.AUTH_TYPE = (int)AuthType.Domain;
 
-            // Update Global variable
-            Global.ID = Global.KEY01 = tbID.Text;
-            Global.PASSWORD = Global.KEY02 = tbPassword.Text;
+                #region Domain
+                // Update global variable
+                Global.DOMAIN_PERMIT = DomainPermit;
+                Global.USER_DOMAIN_NAME = Global.KEY01 = Global.GetUserDomainName();
+                Global.DOMAIN_NAME = Global.KEY02 = Global.GetDomainName();
 
-            // Update Rememberme
-            Config.UpdateAppSetting(Config.REMEMBER_ME, isRememberMe.ToString());
+                if (Global.DOMAIN_NAME == "")
+                {
+                    MessageBox.Show("Your are not in Active Domain");
+                    return;
+                }
 
-            Global.METADATA_FILENAME = Global.ID + ".data";
+                // Save to config file
+                Config.UpdateAppSetting(Config.DOMAIN_PERMIT, Global.DOMAIN_PERMIT.ToString());
+
+                #endregion
+            }
+            Config.UpdateAppSetting(Config.AUTH_TYPE, Global.AUTH_TYPE.ToString());
+
+            Main.UpdateAesKey();
         }
 
         #endregion
@@ -100,16 +149,6 @@ namespace DemoHackingApp
         private void rbSomeOneDomain_CheckedChanged(object sender, EventArgs e)
         {
             DomainPermit = (int)DomainPermission.SomeUserInDomain;
-        }
-
-        private void btApplyDomain_Click(object sender, EventArgs e)
-        {
-            // Update global variable
-            Global.DOMAIN_PERMIT = DomainPermit;
-
-            // Save to config file
-            Config.UpdateAppSetting(Config.DOMAIN_PERMIT, Global.DOMAIN_PERMIT.ToString());
-
         }
 
         #endregion
